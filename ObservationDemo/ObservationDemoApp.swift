b5e9c62088dc752ea73fa112b13523f9d94f4022
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-17 14:49:47 +0800

    Initial Commit

---
+++ ObservationDemo/ObservationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationDemoApp.swift
+//  ObservationDemo
+//
+//  Created by Kelvin on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
